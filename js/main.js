f0820e19858abafc89fce6b0198cfa8fdfa9a8a9
Matias <matiasrsolis@yahoo.com.ar>
2020-09-18 15:19:13 -0300

    nav icons

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
     $("#btnMenu").click(function(){
         $(".mainNav").toggleClass("hideMenu");
+        $(".iconMenu").toggleClass("none");
+        $(".iconCloseHide").toggleClass("iconClose")
     });
     
     $(".btnSection").click(function(){
-        
-        $(this).children("ul").slideToggle();
+        $(".subMenu").toggleClass("subMenuView");
+        //$(this).children("ul").slideToggle();
+        $("#submenuRight").toggleClass("none");
+        $(".submenuDownHide").toggleClass("submenuDown");
     });
     
     $('.carousel').carousel({
-      interval: 2000
+      interval: 4000
     })
 });
 
